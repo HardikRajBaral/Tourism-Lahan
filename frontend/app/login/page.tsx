4673ff24987f2a94d2c50d0c7bff95691591387c
HardikRajBaral <hardikrajbaral232@gmail.com>
2026-06-04 14:16:17 +0545

    Implement login form with validation and submission handling; update package dependencies

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,11 +1,25 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { FormSchema, formSchema } from "@/utils/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 export default function LoginPage() {
+
+
+  const handleLogin= async(data:FormSchema)=>{
+    
+    const res= await fetch("/api/v1/users/login",{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(data)
+    })
+    reset()
+  }
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors, isSubmitting },
   } = useForm<FormSchema>({
     resolver:zodResolver(formSchema)
@@ -18,7 +32,26 @@ export default function LoginPage() {
           <p>Welcome to the the site .</p>
         </div>
         <div>
-          <form></form>
+          <form onSubmit={handleSubmit(handleLogin)}>
+            {/*username or email */}
+            <div>
+              <label htmlFor="identifier">Email or Username</label>
+              <input  type='text' id="identifier"  {...register("identifier")} />
+              {errors.identifier && <p>{errors.identifier.message}</p>} 
+            </div>
+            {/*username or email */}
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type='password' id="password" {...register("password")} />
+              {errors.password && <p>{errors.password.message}</p>}
+            </div>
+            {/*submitting button */}
+            <div>
+              <button type="submit" disabled={isSubmitting}>
+                {isSubmitting ? "Logging in..." : "Login"}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
